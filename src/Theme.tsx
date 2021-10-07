81f4683df03dd4b7bc0ab2c198be9bfd078415d4
Gabriel Carneiro <gabriel.carneiro.castro@gmail.com>
2021-10-07 13:16:02 -0300

    adicionando um arquivo de tema

---
+++ src/Theme.tsx
@@ -0,0 +1,16 @@
+const theme = {
+  colors: {
+    primary: '#0047BB',
+    secondary: '#F06400',
+    danger: '#D64B45',
+    success: '#79C300',
+    white: '#FFFFFF',
+    gray000: '#E5E5E5',
+    gray001: '#F5F8FA',
+    gray002: '#657786',
+    gray003: '#14171A',
+  },
+};
+
+export default theme;
+export type Theme = typeof theme;
