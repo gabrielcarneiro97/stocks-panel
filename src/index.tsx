aef0c860195666a5893b2344a92a7841433d7e03
Gabriel Carneiro <gabriel.carneiro.castro@gmail.com>
2021-10-12 12:21:38 -0300

    adicionei uma lib para notificações

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { Provider as StoreProvider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import theme from 'Theme';
 import App from 'App';
@@ -12,12 +13,29 @@ const GlobalStyle = createGlobalStyle`
   * {
     font-family: 'Open Sans', sans-serif;
   }
+
+  :root {
+    --toastify-color-success: ${(props) => props.theme.colors.success};
+    --toastify-color-warning: ${(props) => props.theme.colors.secondary};
+    --toastify-color-error: ${(props) => props.theme.colors.danger};
+  }
 `;
 
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider store={store}>
       <ThemeProvider theme={theme}>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         <GlobalStyle />
         <App />
       </ThemeProvider>
